624327172f5bfb45ff1021a1c0f0b616fedf4d6b
Bonnet <theo.bonnet1@etu.unice.fr>
2019-06-04 23:17:28 +0200

    add polling for auth

--- clientSide/src/Client.java
+++ clientSide/src/Client.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.UUID;
@@ -8,11 +10,20 @@ public class Client {
         Registry reg = LocateRegistry.getRegistry(2001);
         Authentification mod = (Authentification)reg.lookup("AuthInterface");
         UUID token = null;
-        try {
-            token = mod.authentificate("login","password");
-            System.out.println(token);
-        }catch(Exception e){
-            System.out.println("authentification failed");
+        while(token==null) {
+            try {
+                System.out.print("login : ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String login = reader.readLine();
+                System.out.print("password : ");
+                String password = reader.readLine();
+
+
+                token = mod.authentificate(login, password);
+                System.out.println(token);
+            } catch (Exception e) {
+                System.out.println("authentication failed");
+            }
         }
         IService service = (IService) reg.lookup("Service");
         try{
