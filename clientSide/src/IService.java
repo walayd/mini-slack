0e188dddab031197be36d928c9c5097e89c81c1e
bonnet <theo.bonnet1@etu.unice.fr>
2019-06-04 20:32:39 +0200

    commit

---
+++ clientSide/src/IService.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+public interface IService extends Remote, Serializable {
+    String helloWorld(UUID token) throws RemoteException;
+}
